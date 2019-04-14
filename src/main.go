aa8c56c1fa709b3e8611a6c0ca1488ce2a593875
piyush <bhangalepiyush@gmail.com>
2019-04-14 14:18:59 +0530

    Done For Today

--- src/main.go
+++ src/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/kataras/iris"
 )
+
+const maxSize = 5 << 30
 
 func main() {
 	app := iris.New()
@@ -13,5 +18,35 @@ func main() {
 		ctx.View("index.html")
 	})
 
+	app.Post("/upload", iris.LimitRequestBodySize(maxSize), func(ctx iris.Context) {
+		file, info, err := ctx.FormFile("file[]")
+
+		if err != nil {
+			ctx.StatusCode(iris.StatusInternalServerError)
+			ctx.HTML("<p> An Error Occured: " + err.Error() + "</p>")
+			return
+		}
+
+		defer file.Close()
+
+		fileName := info.Filename
+
+		out, err := os.OpenFile("./uploads/"+fileName,
+			os.O_WRONLY|os.O_CREATE, 0666)
+
+		if err != nil {
+			ctx.StatusCode(iris.StatusInternalServerError)
+			ctx.HTML("<p> An Error Occured: " + err.Error() + "</p>")
+			return
+		}
+
+		defer out.Close()
+
+		io.Copy(out, file)
+
+		ctx.JSON(iris.Map{
+			"success":  true,
+			"fileName": fileName})
+	})
 	app.Run(iris.Addr(":241"))
 }
